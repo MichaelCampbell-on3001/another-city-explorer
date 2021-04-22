772a3bcae90cf9792eedcf0029386bf854a215fa
MichaelCampbell2020 <cam3000@gmail.com>
2021-04-22 15:12:04 -0700

    completed form templated on app js

--- src/App.js
+++ src/App.js
@@ -107,40 +107,15 @@ render(){
       >
         <h2>The city you searched was featured in these movies!</h2>
         </Navbar>
-        
 
+        <Movies movies={this.state.movies}/>
+        </>
     )}
-
-
-
-
-
-
+    
     </Container>
-  )
-}
-
-}
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
   );
+}
+
 }
 
 export default App;
