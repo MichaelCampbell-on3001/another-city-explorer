a661dbfad9ba73660df44c1d4b799781c4d6ff05
MichaelCampbell2020 <cam3000@gmail.com>
2021-04-22 13:59:43 -0700

    added super

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-import logo from './logo.svg';
+import React from "react";
+import { Container, Form, Button, Card, Navbar, ListGroup } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
 import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchQuery: "",
+      location: {},
+      weather: [],
+      movies: [],
+      error: {},
+      isError: false,
+    };
+  }
+
+  
+}
 
 function App() {
   return (
