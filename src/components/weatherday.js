be3d86c1e3ca694de8e4fa997dcae6dfdbc16de2
MichaelCampbell2020 <cam3000@gmail.com>
2021-04-25 19:13:32 -0700

    cleaning up city explorer

---
+++ src/components/weatherday.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Card from 'react=bootstrap/Card';
+
+
+export default class errorcard extends React.Component {
+  render() {
+    return(
+      <
+    )
+  }
+}
